dbec8530801022a1f3a416312e148c058bb5a116
legiorex <batyushev@gmail.com>
2018-10-07 22:17:16 +1000

    Тестовое SPA

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Создаю тестовое SPA</h1>
+                <ul className = 'header'>
+                    <li><a href="/">Главная</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
